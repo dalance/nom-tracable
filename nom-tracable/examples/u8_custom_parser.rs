273b9d6621124deb61acf2c3741e37d011220e38
dalance <dalance@gmail.com>
2020-07-09 23:53:11 +0900

    Refine u8_custom_parser example #4

--- nom-tracable/examples/u8_custom_parser.rs
+++ nom-tracable/examples/u8_custom_parser.rs
@@ -1,6 +1,8 @@
 use nom::branch::*;
 use nom::character::complete::*;
-use nom::{AsBytes, IResult, InputIter, Offset, Slice};
+#[cfg(feature = "trace")]
+use nom::{AsBytes, Offset};
+use nom::{IResult, InputIter, Slice};
 use nom_locate::LocatedSpan;
 use nom_tracable::{cumulative_histogram, histogram, tracable_parser, TracableInfo};
 #[cfg(feature = "trace")]
@@ -12,26 +14,24 @@ pub struct Span<'a>(LocatedSpan<&'a [u8], TracableInfo>);
 #[cfg(feature = "trace")]
 impl<'a> HasTracableInfo for Span<'a> {
     fn get_tracable_info(&self) -> TracableInfo {
-        self.0.extra.get_tracable_info()
+        self.0.get_tracable_info()
     }
 
     fn set_tracable_info(mut self, info: TracableInfo) -> Self {
-        self.0.extra = self.0.extra.set_tracable_info(info);
+        self.0 = self.0.set_tracable_info(info);
         self
     }
 }
 #[cfg(feature = "trace")]
 impl<'a> Tracable for Span<'a> {
-    fn inc_depth(self) -> Self {
-        let info = self.get_tracable_info();
-        let info = info.depth(info.depth + 1);
-        self.set_tracable_info(info)
+    fn inc_depth(mut self) -> Self {
+        self.0 = self.0.inc_depth();
+        self
     }
 
-    fn dec_depth(self) -> Self {
-        let info = self.get_tracable_info();
-        let info = info.depth(info.depth - 1);
-        self.set_tracable_info(info)
+    fn dec_depth(mut self) -> Self {
+        self.0 = self.0.dec_depth();
+        self
     }
 
     // Customize fragment format for &[u8]
@@ -48,7 +48,7 @@ impl<'a> Tracable for Span<'a> {
     }
 
     fn header(&self) -> String {
-        format!("{:<8} : {}", "offset", "fragment")
+        self.0.header()
     }
 }
 impl<'a> InputIter for Span<'a> {
@@ -57,60 +57,27 @@ impl<'a> InputIter for Span<'a> {
     type IterElem = std::iter::Map<std::slice::Iter<'a, Self::Item>, fn(&u8) -> u8>;
     #[inline]
     fn iter_indices(&self) -> Self::Iter {
-        self.0.fragment().iter_indices()
+        self.0.iter_indices()
     }
     #[inline]
     fn iter_elements(&self) -> Self::IterElem {
-        self.0.fragment().iter_elements()
+        self.0.iter_elements()
     }
     #[inline]
     fn position<P>(&self, predicate: P) -> Option<usize>
     where
         P: Fn(Self::Item) -> bool,
     {
-        self.0.fragment().position(predicate)
+        self.0.position(predicate)
     }
     #[inline]
     fn slice_index(&self, count: usize) -> Option<usize> {
-        self.0.fragment().slice_index(count)
+        self.0.slice_index(count)
     }
 }
 impl<'a> Slice<std::ops::RangeFrom<usize>> for Span<'a> {
     fn slice(&self, range: std::ops::RangeFrom<usize>) -> Self {
-        if (|range: &std::ops::RangeFrom<usize>| range.start == 0)(&range) {
-            return self.clone();
-        }
-        let next_fragment = self.0.fragment().slice(range);
-        let consumed_len = self.0.fragment().offset(&next_fragment);
-        if consumed_len == 0 {
-            let span = unsafe {
-                LocatedSpan::new_from_raw_offset(
-                    self.0.location_offset(),
-                    self.0.location_line(),
-                    next_fragment,
-                    self.0.extra.clone(),
-                )
-            };
-            return Span(span);
-        }
-
-        let consumed = self.0.fragment().slice(..consumed_len);
-        let next_offset = self.0.location_offset() + consumed_len;
-
-        let consumed_as_bytes = consumed.as_bytes();
-        let iter = memchr::Memchr::new(b'\n', consumed_as_bytes);
-        let number_of_lines = iter.count() as u32;
-        let next_line = self.0.location_line() + number_of_lines;
-
-        let span = unsafe {
-            LocatedSpan::new_from_raw_offset(
-                next_offset,
-                next_line,
-                next_fragment,
-                self.0.extra.clone(),
-            )
-        };
-        Span(span)
+        Span(self.0.slice(range))
     }
 }
 
